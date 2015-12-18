be182bb3547f99c71587207fde3477f6a5b2e475
victornicolet <victor.nicolet.0@gmail.com>
2015-12-18 14:56:11 +0100

    Demo : some positives

---
+++ demo.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+clr="\e[0;30m\e[47m"
+def="\e[0m"
+echo -e "${clr}Demo model checker pour un sous ensemble de lustre ${def}"
+echo -e "${clr}Exemple 1 : flots de booléens alternant true/false ${def}"
+more examples/positives/ex01.lus
+read touche
+src/lmoch examples/positives/ex01.lus check1 -v
+read touche
+echo -e "${clr}Exemple 2: if/then/else + node inlining ${def}"
+more examples/positives/ex02.lus
+read touche
+src/lmoch examples/positives/ex02.lus  check2 -v
+read touche
+echo -e "${clr}Exemple 3: entiers ${def}"
+more examples/positives/ex003.lus
+read touche
+src/lmoch examples/positives/ex003.lus check -v
+echo ""
+echo "************"
+echo -e "${clr} FIN ${def}"
